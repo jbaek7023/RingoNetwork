f0f330d6e60c11086f897628097785f5c9214f6f
tjbright1 <tbright32@gatech.edu>
2018-04-23 18:49:33 -0400

    forwarding fixed

--- ringo.py
+++ ringo.py
@@ -165,11 +165,11 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
             if incoming_seq_number != expected_packet:
                 print('Unexpected packet: ' + str(incoming_seq_number))
 
-                try:
+                if incoming_seq_number <= expected_packet or expected_packet == 0:
+                
                     print("sending ack for " + str(incoming_seq_number)+ " anyway")
                     socketo.sendto(pckt_ack.encode('utf-8'), self.client_address)
-                except:
-                    pass
+                    
 
 
 
@@ -207,11 +207,11 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
                             pass
 
                         init_window(socketo, filename, file_length)
-            print('sending ack ' + str(incoming_seq_number) + "; still want all " + str(file_length))
-            try:
-                socketo.sendto(pckt_ack.encode('utf-8'), self.client_address)
-            except:
-                pass
+                print('sending ack ' + str(incoming_seq_number) + "; still want all " + str(file_length))
+                try:
+                    socketo.sendto(pckt_ack.encode('utf-8'), self.client_address)
+                except:
+                    pass
         elif keyword == "file_ack": # this is an ack for the piece of the file we sent
             # data = json_obj['data']
             ack_number = json_obj['ack_number']
